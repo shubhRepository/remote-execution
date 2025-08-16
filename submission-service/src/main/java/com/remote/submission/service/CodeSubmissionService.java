c746eda42bc53ca73be46ea50cf96ca5a3f538fe
Shubham Maheshwari <academicShubh@gmail.com>
2025-08-16 18:12:22 +0530

    add code to the queue

--- submission-service/src/main/java/com/remote/submission/service/CodeSubmissionService.java
+++ submission-service/src/main/java/com/remote/submission/service/CodeSubmissionService.java
@@ -1,7 +1,9 @@
 package com.remote.submission.service;
 
+import com.remote.submission.config.Constants;
 import com.remote.submission.model.CodeSubmission;
 import com.remote.submission.repository.CodeSubmissionRepository;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +14,12 @@ import java.util.UUID;
 public class CodeSubmissionService {
 
     private final CodeSubmissionRepository codeSubmissionRepository;
+    private final RabbitTemplate rabbitTemplate;
 
     @Autowired
-    public CodeSubmissionService(CodeSubmissionRepository codeSubmissionRepository) {
+    public CodeSubmissionService(CodeSubmissionRepository codeSubmissionRepository, RabbitTemplate rabbitTemplate) {
         this.codeSubmissionRepository = codeSubmissionRepository;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     public CodeSubmission handleCodeSubmission(CodeSubmission codeSubmission) {
@@ -25,6 +29,8 @@ public class CodeSubmissionService {
         }
         codeSubmission.setCodeContent(Base64.getEncoder().encodeToString(codeSubmission.getCodeContent().getBytes()));
         codeSubmissionRepository.save(codeSubmission);
+        rabbitTemplate.convertAndSend(Constants.FILE_EXECUTION_EXCHANGE, Constants.FILE_EXECUTION_ROUTING_KEY,
+                codeSubmission);
         return codeSubmission;
     }
 }
