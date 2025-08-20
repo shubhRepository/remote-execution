57f3708e7f78d170b0f6464272986c822347b842
Shubham Maheshwari <academicShubh@gmail.com>
2025-08-20 14:24:11 +0530

    add security configurations in api gateway

--- api-gateway-service/src/main/java/com/remote/apigateway/controller/AuthController.java
+++ api-gateway-service/src/main/java/com/remote/apigateway/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.remote.apigateway.controller;
 import com.remote.apigateway.model.User;
 import com.remote.apigateway.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,7 +17,8 @@ public class AuthController {
     }
 
     @PostMapping("/signup")
-    public User signup(@RequestBody User user) {
-        return userService.save(user);
+    public ResponseEntity<String> signup(@RequestBody User user) {
+        userService.save(user);
+        return ResponseEntity.ok("User registered successfully");
     }
 }
--- api-gateway-service/src/main/java/com/remote/apigateway/service/UserService.java
+++ api-gateway-service/src/main/java/com/remote/apigateway/service/UserService.java
@@ -3,6 +3,7 @@ package com.remote.apigateway.service;
 import com.remote.apigateway.model.User;
 import com.remote.apigateway.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -10,13 +11,19 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public User save(User newUser) {
-        return userRepository.save(newUser);
+    public void save(User newUser) throws IllegalArgumentException {
+        if (userRepository.findByUsername(newUser.getUsername()).isPresent()) {
+            throw new IllegalArgumentException("User already exists");
+        }
+        newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
+        userRepository.save(newUser);
     }
 }
